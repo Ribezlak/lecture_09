43590cf15d2399cd6ad4902e96c7505e1ebdd7f1
Martin Suriak <martin.suriak258@gmail.com>
2022-04-11 12:31:54 +0200

    Positions and counter

--- searching.py
+++ searching.py
@@ -19,9 +19,26 @@ def read_data(file_name, field):
         return data[field]
 
 
+def linear_search(sequence, searched_number):
+    positions = []
+    count = 0
+
+    for i, number in enumerate(sequence):
+        if number == searched_number:
+            count += 1
+            positions.append(i)
+
+    output = dict()
+    output["positions"] = positions
+    output["count"] = count
+    return output
+
+
 def main():
     unordered_numbers = read_data("sequential.json", "unordered_numbers")
-    print(unordered_numbers)
+
+    search_output_dict = linear_search(unordered_numbers, 0)
+    print(search_output_dict)
 
 
 if __name__ == '__main__':
